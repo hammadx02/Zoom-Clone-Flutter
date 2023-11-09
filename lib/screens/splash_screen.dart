0b9c562b8594709d43552ecd60bcb69897e7b687
hammadx02 <hammadaali02@gmail.com>
2023-11-09 14:22:06 +0500

    updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,8 +10,8 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(),
+    return  const Scaffold(
+      
     );
   }
 }
